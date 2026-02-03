090628762d2ce1cfc258c050f6b3f3b253eddf37
Gerald Low <gerald@b-k-k.com.au>
2026-02-03 12:39:16 +1100

    Fix TypeScript error in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,18 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        fontFamily: {
+      fontFamily: {
         // This connects your arbitrary CSS variable to the Tailwind class 'font-monument'
         monument: ["var(--font-monument)", "sans-serif"],
       },
     },
   },
-  plugins: [],
-};
+  plugins: [] as const,
+} satisfies Config;
 export default config;
